5045825e398179e69bb06c1fd4cc8371791ea6c0
marsmoon <you@example.com>
2017-07-10 23:47:55 +0200

    guns

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+	GameObject gunTip;
+
+
+	// Use this for initialization
+	void Start () {
+		gunTip = GameObject.Find ("GunTip");
+		if (gunTip == null)
+			Debug.LogWarning ("No gunTip!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
