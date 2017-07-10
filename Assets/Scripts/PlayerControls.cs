f576e96f8a7673c66525a84d871659cb20171684
waterbuckit <adamroyal232@yahoo.com>
2017-07-10 22:23:48 +0100

    Commented code and added sprites

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -18,13 +18,15 @@ public class PlayerControls : MonoBehaviour {
 	{
 		setGunRotation (getCurrentMousePos ());
 	}
+	// sets the current gun rotation based on the current mouse position
  	private void setGunRotation(Vector3 currentMousePos){
-		Vector3 diff = getCurrentMousePos() - transform.position;
+		Vector3 diff = currentMousePos - transform.position;
 		diff.Normalize();
-
+		// z and x because our axis are fucked
 		float rot_z = Mathf.Atan2(diff.z, diff.x) * Mathf.Rad2Deg;
 		gun.transform.rotation = Quaternion.Euler(90, 0f, rot_z - 90);
 	}
+	// gets the current mouse position 
 	private Vector3 getCurrentMousePos(){
 		Vector3 currentPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 		return currentPos;
